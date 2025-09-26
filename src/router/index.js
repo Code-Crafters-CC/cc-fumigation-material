eb40931dc1504bd2e272fff6df7bc964036c216c
RicardoWebProject <luisrijhn09@gmail.com>
2025-09-25 21:54:23 -0600

    Added price calculator

--- src/router/index.js
+++ src/router/index.js
@@ -31,6 +31,7 @@ import AboutFumigationSystem from "../views/LandingPages/components/AboutFumigat
 import Mission from "../views/LandingPages/components/Mission.vue";
 import Vision from "../views/LandingPages/components/Vision.vue";
 
+import PriceCalculator from "../views/LandingPages/components/PriceCalculator.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
